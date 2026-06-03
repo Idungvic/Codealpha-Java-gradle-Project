9cd98dcce402028cb1c7c81a56b17d030fa22a2e
Eddie <idungvictor@gmail.com>
2026-06-03 10:25:24 +0100

    Initial Gradle project setup

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello CodeAlpha!");
+        System.out.println("Java Application Built With Gradle");
+    }
+}
